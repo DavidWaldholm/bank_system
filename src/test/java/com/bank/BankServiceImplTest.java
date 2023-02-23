4f41b201db1c39d7ecace23ddcdfe8a41c7ecd30
DavidWaldholm <113913489+DavidWaldholm@users.noreply.github.com>
2023-02-24 00:01:42 +0100

    Adding test for BankServiceImpl

--- src/test/java/com/bank/BankServiceImplTest.java
+++ src/test/java/com/bank/BankServiceImplTest.java
@@ -1,4 +1,56 @@
 package com.bank;
 
+import com.bank.service.impl.BankServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class BankServiceImplTest {
+    BankServiceImpl bankService;
+    BankAccount bankAccount;
+
+    @Before
+    public void initialize() {
+        bankService = new BankServiceImpl();
+        bankAccount = new BankAccount("David", "1");
+    }
+
+
+    @Test
+    public void testWithdrawNormal() {
+        bankAccount.setBalance(200.0);
+        bankService.withdraw(bankAccount, 100.0);
+        Assertions.assertEquals(100.0, bankAccount.getBalance());
+        Assertions.assertEquals(-100.0, bankAccount.getPrevTrans());
+
+
+    }
+
+    @Test
+    public void testWithdrawBiggerAmountThanCurrentBalance() {
+        bankAccount.setBalance(200.0);
+        bankService.withdraw(bankAccount, 210.0);
+        Assertions.assertEquals(200.0, bankAccount.getBalance());
+
+
+    }
+
+
+    @Test
+    public void testDepositNormal() {
+        bankService.deposit(bankAccount, 200.0);
+        Assertions.assertEquals(200.0, bankAccount.getBalance());
+        Assertions.assertEquals(200, bankAccount.getPrevTrans());
+
+
+    }
+
+    @Test
+    public void testDepositWithNegativeAmount() {
+        bankService.deposit(bankAccount, 200.0);
+        bankService.deposit(bankAccount, -200.0);
+        Assertions.assertEquals(200, bankAccount.getBalance());
+
+    }
+
 }
