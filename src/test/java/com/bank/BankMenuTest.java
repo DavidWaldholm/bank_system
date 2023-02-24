896902fd11363a178b7e745bfd144dd451cee354
davidwaldholm <davidwaldholm@yahoo.se>
2023-02-24 16:20:39 +0100

    Added bankMenuTest and did some small tweaks for code

--- src/test/java/com/bank/BankMenuTest.java
+++ src/test/java/com/bank/BankMenuTest.java
@@ -1,4 +1,56 @@
 package com.bank;
 
+import com.bank.service.BankService;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
+
 public class BankMenuTest {
+    BankService bankService;
+    BankAccount bankAccount;
+    BankMenu bankMenu;
+
+    @Test
+    public void testWithdrawMenu() {
+        bankService = mock(BankService.class);
+        bankAccount = new BankAccount("BankAccountTest", "1");
+        Scanner sc = mock(Scanner.class);
+        bankMenu = new BankMenu(bankService, bankAccount, sc);
+        double amountWithdraw = 500.0;
+        when(sc.next()).thenReturn("c", "e");
+        when(sc.nextDouble()).thenReturn(500.0);
+        bankMenu.menu();
+        verify(bankService, atLeast(1)).withdraw(bankAccount, amountWithdraw);
+    }
+
+
+    @Test
+    public void testDepositMenu() {
+        bankService = mock(BankService.class);
+        bankAccount = new BankAccount("BankAccountTest", "1");
+        Scanner sc = mock(Scanner.class);
+        bankMenu = new BankMenu(bankService, bankAccount, sc);
+        when(sc.next()).thenReturn("b", "e");
+        when(sc.nextDouble()).thenReturn(500.0);
+        bankMenu.menu();
+        verify(bankService, atLeast(1)).deposit(bankAccount, 500.0);
+    }
+
+    @Test
+    public void testBankMenuShouldNotThrowRunTimeException() {
+        bankService = mock(BankService.class);
+        Scanner sc = mock(Scanner.class);
+        bankAccount = new BankAccount("BankAccountTest", "1");
+        bankMenu = new BankMenu(bankService, bankAccount, sc);
+        doThrow(new RuntimeException()).when(bankService).withdraw(any(), anyDouble());
+        assertThrows(RuntimeException.class, () -> bankService.withdraw(any(), anyDouble()));
+        when(sc.next()).thenReturn("c", "e");
+        when(sc.nextDouble()).thenReturn(0.0);
+        bankMenu.menu();
+        assertDoesNotThrow(() -> bankMenu.menu());
+    }
 }
